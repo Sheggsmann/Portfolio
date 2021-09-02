10977b5f90a623ef8695efef4f2f7b4dd636c5b3
Sheggsmann <promisesheggs@gmail.com>
2021-09-02 21:28:48 +0100

    Added Portfolio and Contact Pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+    
+    const scrollBtn = document.querySelector('.scroll-top');
+    window.addEventListener('scroll', (e) => {
+        if (window.scrollY > 200) {
+            scrollBtn.classList.add('show');
+        } else {
+            scrollBtn.classList.remove('show');
+        }
+    })
+
+
+
     // Testimonial Carousel
     $('.testimonials-carousel').owlCarousel({
         loop: true,
