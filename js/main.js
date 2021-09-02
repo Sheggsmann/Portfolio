7b221699f3670216cd40b6ea18e9669f2c589479
Sheggsmann <promisesheggs@gmail.com>
2021-09-02 14:07:11 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    // Testimonial Carousel
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 2,
+            }
+        }
+    })
+
+})
+
+
